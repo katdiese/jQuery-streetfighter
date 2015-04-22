4f02cb1b2d90e85f96027311ab1b9d7149f1375b
Kathy Dieser <katdiese@gmail.com>
2015-04-21 19:54:52 -0600

    added ryu-cool pose jQuery

--- js/app.js
+++ js/app.js
@@ -11,12 +11,36 @@ $(document).ready(function(){
     //play hadouken sound
     $('.ryu-ready').hide();
     $('.ryu-throwing').show();
-    $('.hadouken').show();
-    //show hadouken and animate it to the right of the screen
+    $('.hadouken').finish().show().animate(
+      {'left': '1020px'},
+        500,
+        function() {
+          $(this).hide();
+          $(this).css('left', '520px');
+          }
+      );
   })
   .mouseup(function(){
+    playHadouken();
     $('.ryu-ready').show();
     $('.ryu-throwing').hide();
     //ryu goes back to his ready position
   });
+  $(document).keydown(function(e) {
+    if (e.which == 88) {
+      $('.ryu-still').hide();
+      $('.ryu-cool').show();
+    }
+  })
+  .keyup(function(e){
+    if (e.which == 88) {
+      $('.ryu-still').show();
+      $('.ryu-cool').hide();
+    }
+  });
 });
+function playHadouken () {
+  $('#hadouken-sound')[0].volume=0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
